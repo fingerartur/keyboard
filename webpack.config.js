84d0902d78003de4a5eb595068474b3d5496c2ce
Artur Finger <artur.finger@nangu.tv>
2021-07-08 11:12:30 +0200

    fixed build

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = (env) => ({
+    devtool: false,
+    mode: 'production',
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    output: {
+        filename: 'index.js',
+        globalObject: 'this',
+        library: 'library',
+        libraryTarget: 'umd',
+        path: path.resolve(__dirname, 'dist'),
+        umdNamedDefine: true,
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+})
